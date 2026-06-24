83b549f110d2ef270e4f2d1e26004c3cd419e883
DarriEy <darri.eythorsson@ucalgary.ca>
2026-06-24 18:24:29 +0000

    Resolve port debt in src/CLM/BioGeoChem/FATES/Fire.hs via Ralph Loop

--- src/CLM/BioGeoChem/FATES/Fire.hs
+++ src/CLM/BioGeoChem/FATES/Fire.hs
@@ -1,3 +1,45 @@
-module CLM.BioGeoChem.FATES.Fire where
+{-# LANGUAGE BangPatterns #-}
+-- | FATES Fuel and Fire Dynamics
+-- Ported from FatesFuelMod.F90 & FatesFuelClassesMod.F90
+module CLM.BioGeoChem.FATES.Fire
+  ( -- * Fuel Classes
+    numFuelClasses
+  , idxTwigs
+  , idxSmallBranches
+  , idxLargeBranches
+  , idxTrunks
+  , idxDeadLeaves
+  , idxLiveGrass
+  
+  -- * Calculations
+  , calculateFuelMoistureNesterov
+  ) where
 
--- TODO: Port FATES fuel & fire models from FatesFuelMod.F90 & FatesFuelClassesMod.F90
+import qualified Data.Vector.Unboxed as VU
+
+-- | Total number of fuel classes
+numFuelClasses :: Int
+numFuelClasses = 6
+
+-- | 0-based indices for fuel classes
+idxTwigs, idxSmallBranches, idxLargeBranches, idxTrunks, idxDeadLeaves, idxLiveGrass :: Int
+idxTwigs         = 0
+idxSmallBranches = 1
+idxLargeBranches = 2
+idxTrunks        = 3
+idxDeadLeaves    = 4
+idxLiveGrass     = 5
+
+-- | Update fuel moisture using the Nesterov Index.
+-- Maps to CalculateFuelMoistureNesterov in FatesFuelMod.F90.
+calculateFuelMoistureNesterov
+  :: VU.Vector Double -- ^ sav_fuel surface area to volume ratio of all fuel types [/cm] (length 6)
+  -> Double           -- ^ drying_ratio
+  -> Double           -- ^ NI Nesterov Index
+  -> VU.Vector Double -- ^ moisture of litter [m3/m3] (length 6)
+calculateFuelMoistureNesterov !savFuel !dryingRatio !ni =
+  VU.generate numFuelClasses $ \i ->
+    let !alphaFmc = if i == idxLiveGrass
+                      then (savFuel VU.! idxTwigs) / dryingRatio
+                      else (savFuel VU.! i) / dryingRatio
+    in exp (-1.0 * alphaFmc * ni)
