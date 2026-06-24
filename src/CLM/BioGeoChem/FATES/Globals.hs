ebb1bfd57240a32c14596fab6d30e0e624266d25
DarriEy <darri.eythorsson@ucalgary.ca>
2026-06-24 18:24:41 +0000

    Resolve port debt in src/CLM/BioGeoChem/FATES/Globals.hs via Ralph Loop

--- src/CLM/BioGeoChem/FATES/Globals.hs
+++ src/CLM/BioGeoChem/FATES/Globals.hs
@@ -1,3 +1,29 @@
-module CLM.BioGeoChem.FATES.Globals where
+-- | FATES Global variables and utility functions
+module CLM.BioGeoChem.FATES.Globals
+  ( fatesLog
+  , fatesGlobalVerbose
+  , fatesEndrun
+  , fatesWarn
+  ) where
 
--- TODO: Port FATES globals from FatesGlobals.F90
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
+
+-- | Returns log file descriptor unit number
+fatesLog :: Int
+fatesLog = 6
+
+-- | Verbose logging flag
+fatesGlobalVerbose :: Bool
+fatesGlobalVerbose = False
+
+-- | End FATES run on error
+fatesEndrun :: String -> IO a
+fatesEndrun msg = do
+  hPutStrLn stderr $ "ENDRUN: " ++ msg
+  exitFailure
+
+-- | Print a warning message
+fatesWarn :: Int -> String -> IO ()
+fatesWarn _id msg = do
+  hPutStrLn stderr $ "FATES Warning: " ++ msg
